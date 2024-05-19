9badef6d1f4a92ce3b287c5ed98cf89bf0d1d91b
Ayman Alkhawaleda <ayman.khawaleda@gmail.com>
2024-05-19 23:59:49 +0400

    Add the main backend application file

---
+++ SpringBackend/src/main/java/cc/madis/DeviceAPI/DeviceApiApplication.java
@@ -0,0 +1,13 @@
+package cc.madis.DeviceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceApiApplication.class, args);
+    }
+
+}
